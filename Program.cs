23c807db3c383e71a38c07dc9a62df33f8088de7
lawrindovsk <gustavo.laurindo.santos@gmail.com>
2025-01-02 16:15:51 -0300

    version 0.1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TextEditor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");//Obrigatório.
+
+        Console.WriteLine("Bem vindo ao Editor de Texto!");
+    }
+}
